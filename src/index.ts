a37a854ddf6454daeed036f65b75679abfb4d408
Lasse Sprengel <lasse.sprengel@gmail.com>
2017-05-26 17:49:53 +0200

    created some streams

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('success')
+import { Transform } from 'stream';
+
+export class IAudioFormat {
+    float: boolean = false;
+}
+
+export class AudioAvsFormater extends Transform {
+    constructor(private inputFormat: IAudioFormat) {
+        super();
+    }
+
+    _write(chunk: Buffer, encoding: any, callback: Function) {
+        this.getSamples(chunk);
+        callback();
+    }
+
+    getSamples(chunk: Buffer) {
+        if (this.inputFormat.float) {
+
+        } else {
+
+        }
+    }
+}
+
+class ResampleStream extends Transform {
+
+}
